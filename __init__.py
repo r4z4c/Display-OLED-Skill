ea2104e9b01467598fbf0e7ee831e68e4707e06b
r4z4c <usia.mueller@googlemail.com>
2018-06-06 16:47:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,16 +10,14 @@ LOGGER = getLogger(__name__)
 class ShowOnDisplaySkill(MycroftSkill):
 
 	def __init__(self):
-        	super(ShowOnDisplaySkill, self).__init__(name="ShowOnDisplaySkill")
-		self.be_friendly = True
+		super(ShowOnDisplaySkill, self).__init__(name="ShowOnDisplaySkill")
 
 	def initialize(self):
-		show_on_display_intent = IntentBuilder("ShowDisplayIntent").
-            		require("ShowOnDisplay").build()
-        	self.register_intent(show_on_display_intent, self.handle_show_on_display_intent)
+		show_on_display_intent = IntentBuilder("ShowDisplayIntent").require("ShowOnDisplay").build()
+		self.register_intent(show_on_display_intent, self.handle_show_on_display_intent)
 
 	def handle_show_on_display_intent(self, message):
-        	self.speak_dialog("is.okay")
+		self.speak_dialog("is.okay")
 		
 def create_skill():
     return ShowOnDisplaySkill()
