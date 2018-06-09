1832d795abb3b14ce44bf240cd086f4ce2c450ec
r4z4c <usia.mueller@googlemail.com>
2018-06-09 11:46:00 +0200

    Update TIME.py

--- TIME.py
+++ TIME.py
@@ -5,26 +5,25 @@ class theTime:
   def __init__(self):
     self.name = "theTime"
     
-  def currentTime(stringa, stringb):
+  def currentTime(self):
     now = datetime.datetime.now()
-    nowyear = now.year
-    nowmonth = now.month
-    nowday = now.day
+    
     nowhour = now.hour
     nowminute = now.minute
     nowsecond = now.second
-    nowTime = str(nowhour).zfill(2) + ":" + str(nowminute).zfill(2) + ":" + str(nowsecond).zfill(2)
-    nowDate = str(nowday).zfill(2) + "." + str(nowmonth).zfill(2) + "." + str(nowyear)
-    if stringa == "time" and stringb == "date":
-      theTime = nowTime + " " + nowDate
-    elif stringa == "date" and stringb == "time":
-      theTime = nowDate + " " + nowTime
-    elif stringa == "time" and stringb == "":
-      theTime = nowTime
-    elif stringa == "date" and stringb == "":
-      theTime = nowDate
-    else:
-      theTime = now
-    return theTime
     
-  def
+    nowTime = str(nowhour).zfill(2) + ":" + str(nowminute).zfill(2) + ":" + str(nowsecond).zfill(2)
+    
+    return nowTime
+    
+  def currentDate(self):
+    now = datetime.datetime.now()
+    
+    nowyear = now.year
+    nowmonth = now.month
+    nowday = now.day
+
+    nowDate = str(nowday).zfill(2) + "." + str(nowmonth).zfill(2) + "." + str(nowyear)
+    
+    return nowDate
+    
