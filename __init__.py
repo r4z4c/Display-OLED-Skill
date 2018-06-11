747c97fbf4bb68cd1ff2ed42091ea2014d6c8d4d
r4z4c <usia.mueller@googlemail.com>
2018-06-11 18:08:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,25 +6,101 @@ from mycroft import intent_handler
 import threading, sys, time
 from os.path import dirname, abspath
 
-#sys.path.append(abspath(dirname(__file__)))
-#import MAINPY
+sys.path.append(abspath(dirname(__file__)))
+
+import DISPLAY
+import TIME
+
 import RPi.GPIO as GPIO
-
-def ButtonHandler(channel):
-  if GPIO.input(channel):
-    GPIO.output(22, GPIO.HIGH)
-  else:
-    GPIO.output(22, GPIO.LOW)
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22, GPIO.OUT)
-GPIO.output(22, GPIO.HIGH)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(27,GPIO.BOTH,ButtonHandler)
 
 __author__ = 'usia'
 
 LOGGER = getLogger(__name__)
+
+class myDisplay:
+  
+  statechange = true
+  
+  bup = 17
+  bdown = 27
+  bleft = 22
+  bright = 18
+  bset = 23
+  bmenu = 24
+  bsnooze1 = 5
+  bsnooze2 = 6
+  
+  menudepth = 0
+  mainmenu[] = {0,{{2, 2, 2}, {2, 2, 2}, {2, 2}}, {},{1}}
+  
+  editstate = false
+  editdepth = 0
+  
+  theTime = None
+  theDate = None
+  
+  
+  
+  def __init__(self):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.bup, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bup,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bdown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bdown,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bleft, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bleft,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bright, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bright,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bset,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bmenu, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bmenu,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bsnooze1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bsnooze1,GPIO.HIGH,self.ButtonHandler)
+    GPIO.setup(self.bsnooze2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(self.bsnooze2,GPIO.HIGH,self.ButtonHandler)
+  
+  def ButtonHandler(channel):
+    switch(channel) {
+      case self.bup:
+        if self.editstate:
+          
+        else:
+          if mainmenu[0] == 0:
+            if menudepth == 0:
+              mainmenu[0] += 1
+          if mainmenu[0] == 1:
+            if menudepth == 0:
+              mainmenu[0] += 1
+            elif menudepth == 1:
+              if mainmenu[1] == 0:
+                mainmenu[1]
+            elif menudepth == 2
+      
+          elif mainmenu[] == 
+            
+            
+        break;
+      case self.bdown:
+      
+        break;
+      case self.bleft:
+      
+        break;
+      case self.bright:
+      
+        break;
+      case self.bset:
+      
+        break;
+      case self.bmenu:
+      
+        break;
+      case self.bsnooze1:
+    }
+      
+  def mainHandler(self, theButton, showDisp):
+    
 
 class myThread(threading.Thread):
 
