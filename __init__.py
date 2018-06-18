8c8061f94fc4c7a8282cfd175f480af8eedbe831
r4z4c <usia.mueller@googlemail.com>
2018-06-18 20:28:44 +0200

    deleted:    BUTTONS.py 	modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -5,10 +5,14 @@ from mycroft.util.log import getLogger
 from mycroft import intent_handler
 import threading, sys, time
 from os.path import dirname, abspath
+from mycroft.messagebus.client.ws import WebsocketClient
+from mycroft.messagebus.message import Message
+import RPi.GPIO as GPIO
+from subprocess import call
 
 sys.path.append(abspath(dirname(__file__)))
 
-import LED, DISPLAY, BUTTONS
+import LED, DISPLAY
 
 import RPi.GPIO as GPIO
 
@@ -20,7 +24,12 @@ class DisplayOLEDSkill(MycroftSkill):
 
     myLEDs = None
     myDisplay = None
-    myButtons = None
+
+    messagebusClient = WebsocketClient()
+
+    pin1 = 17
+    pin2 = 27
+    pin3 = 22
 
     def __init__(self):
         super(DisplayOLEDSkill, self).__init__(name="DisplayOLEDSkill")
@@ -28,7 +37,30 @@ class DisplayOLEDSkill(MycroftSkill):
         self.myDisplay = DISPLAY.theDisplay()
         self.myLEDs.start()
         self.myDisplay.start()
-        self.myButtons = BUTTONS.theButtons(self.myDisplay, self.myButtons)
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.pin1, GPIO.RISING, callback=self.button_shutdown)
+        GPIO.add_event_detect(self.pin2, GPIO.RISING, callback=self.button_stop_alarm)
+        GPIO.add_event_detect(self.pin3, GPIO.RISING, callback=self.button_stop_alarm)
+
+    def onConnected(self, event=None):
+        self.messagebusClient.emit(Message("recognizer_loop:utterance",data={'utterances': 'cancel alarm'}))
+        self.messagebusClient.close()
+
+    def button_shutdown(self, channel):
+        self.myDisplay.config['show'] = 'off'
+        self.myLEDs.config['show'] = 'off'
+        time.sleep(1)
+        call(['sudo shutdown now'])
+
+    def button_stop_alarm(self, channel):
+        if GPIO.input(self.pin2) and GPIO.input(self.pin3):
+            self.messagebusClient.on('connected', self.onConnected)
+            # This will block until the client gets closed
+            self.messagebusClient.run_forever()
 
     @intent_handler(IntentBuilder("AllOffIntent").require("AllOff"))
     def handle_turn_all_off_intent(self, message):
